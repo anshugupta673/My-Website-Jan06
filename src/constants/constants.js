fb3567f530f0464cf581c900409b6357df575aa1
anshugupta673 <alch.anshu@gmail.com>
2022-01-18 23:52:51 +0530

    cons commit

---
+++ src/constants/constants.js
@@ -0,0 +1,111 @@
+// export const projects = [
+//   { 
+//     title: 'x86_64-decOS',
+//     description: "1. A 64bit operating system x86_64-decOS written entirely from scratch in C/C++ is under constant development to test and implement various OS modules, implement paging memory encryption mechanism and much more.\n2. x86_64-decOS supports 64-bit paging memory model, interrupts and exceptions handling, process scheduling.\n3. A FAT12 file system to read and write data, and supports basic linux system calls implemented.",
+//       image: '/images/decProj-3.png',
+//       tags: ['C', 'C++', 'low-level assembly', 'bash', 'Makefile', 'INTEL x86-architecture'],
+//     source: 'https://github.com/anshugupta673/x86_64-decOS-MSC-KIIT',
+//     visit: 'https://github.com/anshugupta673/x86_64-decOS-MSC-KIIT',
+//     id: 0,
+//   },
+//   {
+//     title: 'MyCrypto-Crypt-TBlock',
+//     description: "1. A-blockchain-powered-cryptocurrency MyCrypto-Crypt-TBlock. Full-stack app using node.js, express, react following the test driven development approach.\n2. Created backend Node JS servers, with an express API and full testing suite.\n3. A small demonstration of server-to-server systems with real-time networking capabilities.",
+//     image: '/images/crypto-4.png',
+//     tags: ['React', 'JavaScript'],
+//     source: 'https://google.com',
+//     visit: 'https://github.com/anshugupta673/MyCrypto-Crypt-TBlock',
+//     id: 1,
+//   },
+//   {
+//     title: 'WebRTC App',
+//     description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+//       image: '/images/3.jpg',
+//       tags: ['React', 'WebRTC'],
+//     source: 'https://google.com',
+//     visit: 'https://google.com',
+//     id: 2,
+//   },
+//   {
+//     title: 'Unichat',
+//     description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+//     image: '/images/4.jpg',
+//     tags: ['React', 'ChatEngine', 'Firebase'],
+//     source: 'https://google.com',
+//     visit: 'https://google.com',
+//     id: 3,
+//   },
+// ];
+
+// export const TimeLineData = [
+//   { year: 2017, text: 'Started my journey', },
+//   { year: 2018, text: 'Worked as a freelance developer', },
+//   { year: 2019, text: 'Founded JavaScript Mastery', },
+//   { year: 2020, text: 'Shared my projects with the world', },
+//   { year: 2021, text: 'Started my own platform', },
+// ];
+
+
+
+export const projects = [
+  { 
+    title: 'x86_64-decOS',
+    description: "1. A 64bit operating system x86_64-decOS written entirely from scratch in C/C++ is under constant development to test and implement various OS modules, implement paging memory encryption mechanism and much more. \n 2. x86_64-decOS supports 64-bit paging memory model, interrupts and exceptions handling, process scheduling. \n 3. A FAT12 file system to read and write data, and supports basic linux system calls implemented.",
+      image: '/images/decProj-3.png',
+      tags: ['C', 'C++', 'low-level assembly', 'bash', 'Makefile', 'INTEL x86-architecture'],
+    source: 'https://github.com/anshugupta673/x86_64-decOS-MSC-KIIT',
+    visit: 'https://github.com/anshugupta673/x86_64-decOS-MSC-KIIT',
+    id: 0,
+  }
+];
+
+export const projects1 = [
+  {
+    title: 'MyCrypto-Crypt-TBlock',
+    description: "1. A-blockchain-powered-cryptocurrency MyCrypto-Crypt-TBlock. Full-stack app using node.js, express, react following the test driven development approach.\n2. Created backend Node JS servers, with an express API and full testing suite.\n3. A small demonstration of server-to-server systems with real-time networking capabilities.",
+    image: '/images/crypto-4.png',
+    tags: ['React', 'JavaScript'],
+    source: 'https://google.com',
+    visit: 'https://github.com/anshugupta673/MyCrypto-Crypt-TBlock',
+    id: 1,
+  }
+];
+
+export const projects2 = [
+  {
+    title: 'Microsoft Student Community MSC-Project Wing Website',
+    description: "A teamp-up project website made for Project Wing @ MSC-KIIT official",
+    image: '/images/proj-wing-1.png',
+    tags: ['React', 'JavaScript'],
+    source: 'https://google.com',
+    visit: 'https://github.com/MSCKIIT/Project-Wing',
+    id: 2,
+  }
+];
+//   {
+//     title: 'WebRTC App',
+//     description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+//       image: '/images/3.jpg',
+//       tags: ['React', 'WebRTC'],
+//     source: 'https://google.com',
+//     visit: 'https://google.com',
+//     id: 2,
+//   },
+//   {
+//     title: 'Unichat',
+//     description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+//     image: '/images/4.jpg',
+//     tags: ['React', 'ChatEngine', 'Firebase'],
+//     source: 'https://google.com',
+//     visit: 'https://google.com',
+//     id: 3,
+//   },
+// ];
+
+export const TimeLineData = [
+  { year: 2019, text: 'Started my journey', },
+  { year: 2020, text: 'Several minor projects, got my hands dirty with Web development, CyberSececurity, Systems Programming. Worked as a web developer @CIT', },
+  { year: 2021, text: 'Cybersecurity domain Lead @MSC-KIIT, Worked as a team lead for x86_64-decOS', },
+  { year: 2022, text: 'Deep dive in cloud automation and DevOps technologies', },
+  { year: 2022, text: '...', },
+];
